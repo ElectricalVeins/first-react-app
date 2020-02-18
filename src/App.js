e627f534ba124b1181deebd8498987207052e95e
eveins <maxmost512@gmail.com>
2020-02-18 15:19:03 +0200

    counter

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/*
 import React, { Component } from 'react';
 import UsersList from './components/UserList';
 
@@ -37,6 +38,61 @@ class App extends Component {
       <UsersList users={usersData}/>
     );
   }
+}
+
+export default App;*/
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      count: 0
+    };
+    console.log('constr');
+  }
+
+  componentDidMount () {
+    console.log('mount');
+  }
+
+  componentDidUpdate (prevProps, prevState, snapshot) {
+    console.log('upd');
+  }
+
+  componentWillUnmount () {
+    console.log('unmount');
+  }
+
+  incrementCounter = () => {
+    this.setState({
+                    count: this.state.count + 1
+                  });
+
+
+  };
+
+  decrementCounter = () => {
+    this.setState({
+                    count: this.state.count - 1
+                  });
+
+  };
+
+  render () {
+    console.log('render');
+    return (<div>
+      <h1>{this.state.count}</h1>
+      <button onClick={this.incrementCounter}>+</button>
+      <button onClick={this.decrementCounter}>-</button>
+      <input onInput={(e)=>{
+        this.setState({count: e.currentTarget.value})
+      }} type="text"/>
+      <button onClick={this.setCount}>Apply</button>
+    </div>);
+  }
+
 }
 
 export default App;
